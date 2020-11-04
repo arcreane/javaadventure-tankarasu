7c87ff636d632596cff37188bdb973c14e9872fc
TanKARASU <karasutan@gmail.com>
2020-11-04 10:08:04 +0100

    type d'attaque aventurier codé en dur

--- src/com/company/Aventurer.java
+++ src/com/company/Aventurer.java
@@ -2,19 +2,31 @@ package com.company;
 
 public class Aventurer {
 
-    public Aventurer(String name, int age) {
-        this.m_sName = name;
-        this.m_iAge = age;
+    public Aventurer() {
 
-        System.out.println(this.m_sName + " - " + this.m_iAge);
     }
 
-    private String m_sName;
-    private int m_iAge;
-    private int m_iHealthPoint = 200;
+    //    State
+    private static int m_iHealthPoint = 200;
+    private static int numberOfFlask = 0;
 
-    void openDoor() {
+    /*void openDoor() {
         // l'ouverture de la porte provoque l'attaque du monstre
         System.out.println("La porte s'ouvre");
+    }*/
+
+    //    Behavior
+    public static void attackWithWaterFlask() {
+        System.out.println("Wizard loose 10pt");
+        System.out.println("Il y a " + numberOfFlask + " flasques d'eau");
+        System.out.println("il vour reste " + m_iHealthPoint + " points de vies");
+        numberOfFlask++;
+    }
+
+    public static void attackWithSword() {
+        System.out.println("Barbarian loose 10pt");
+        System.out.println("10% de chance d'assommer l'agresseur");
+        System.out.println("il vour reste" + m_iHealthPoint + " points de vies");
+
     }
 }
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -1,8 +1,13 @@
 package com.company;
 
 public class Room {
+
+    private static String monster="Type de monstre";
+
     boolean m_bIsOpen=false;
     // if last room and monster beated, treasure is accessible
+
+    // necessaire ?
     boolean m_bAccessTreasure = false;
 
 }
